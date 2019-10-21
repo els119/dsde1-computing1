5b1455d93d60f53a0600e92558cef219a022e25d
Ellie <els119@ic.ac.uk>
2019-10-21 21:47:47 +0100

    using if statements

--- Week-3/numberguessinggame.py
+++ Week-3/numberguessinggame.py
@@ -1,6 +1,9 @@
 import random
 number= random.randint(1,11)
 print('Enter a number between 1 and 10')
-guess=input()
-guess=int(float(guess))
-print(guess==number)
+guess= input()
+if guess==number:
+    print('Guess is correct')
+else:
+    print('Sorry, your guess is incorrect')
+    

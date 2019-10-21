c2aca3fe383e80badd19789affff80b79df4d3e3
Ellie <els119@ic.ac.uk>
2019-10-21 22:19:58 +0100

    Changed to guess is correct

--- Week-3/numberguessinggame.py
+++ Week-3/numberguessinggame.py
@@ -8,12 +8,12 @@ else:
     print('please enter another number')
     guess2=input()
     if guess2==number:
-        print('well done your guess is correct')
+        print('Guess is correct')
     else: 
         print('incorrect, last try')
         guess3=input()
         if guess3==number:
-            print('Congrats, you got it right!')
+            print('Guess is correct')
         else:
             print('Incorrect, you have no more guesses left, the answer was ' + str(number))
     

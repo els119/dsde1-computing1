25bd923edb61186a40d705c47dc9742c5af68be9
Ellie <els119@ic.ac.uk>
2019-10-21 21:55:34 +0100

    Using 3 guesses

--- Week-3/numberguessinggame.py
+++ Week-3/numberguessinggame.py
@@ -5,5 +5,15 @@ guess= input()
 if guess==number:
     print('Guess is correct')
 else:
-    print('Sorry, your guess is incorrect')
+    print('please enter another number')
+    guess2=input()
+    if guess2==number:
+        print('well done your guess is correct')
+    else: 
+        print('incorrect, last try')
+        guess3=input()
+        if guess3==number:
+            print('Congrats, you got it right!')
+        else:
+            print('Incorrect, you have no more guesses left, the answer was ' + str(number))
     

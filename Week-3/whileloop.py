6dcfcbc8a90cfdc6fa4a2cfa53e31f15cb877222
Ellie <els119@ic.ac.uk>
2019-10-21 22:43:36 +0100

    loops

---
+++ Week-3/whileloop.py
@@ -0,0 +1,18 @@
+number=0
+while number<=10:
+    print(number)
+    number= number + 1
+
+x=10
+while x>=0:
+    print(x)
+    x=x-1
+
+y=0
+for y in range(11):
+    print(y)
+
+total=0
+for num in range(51):
+    total=total + num
+print(total)
